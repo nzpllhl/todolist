7a5243522b73f901ecec595b5a028be1ead56946
haili <443812682@qq.com>
2020-04-06 17:02:03 +0800

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,105 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="changeValue" />
+    <a-button type="primary" @click="addToList">添加事项</a-button>
+    <a-list bordered class="dt_list" :dataSource="infoLits">
+      <a-list-item class="dt_item" slot="renderItem" slot-scope="item">
+        <a-checkbox @change="(e)=>{cbState(e,item.id)}" :checked="item.done">{{item.title}}</a-checkbox>
+        <a @click="remove(item.id)">删除</a>
+      </a-list-item>
+      <!--  -->
+      <div slot="footer" class="footer">
+        <span>0条剩余</span>
+        <a-button-group>
+          <a-button :type="valkey === 'all' ? 'primary':'default'" @click="changeList('all')">全部</a-button>
+          <a-button
+            :type="valkey === 'undone' ? 'primary':'default'"
+            @click="changeList('undone')"
+          >未完成{{getDonelength}}条</a-button>
+          <a-button
+            :type="valkey === 'done' ? 'primary':'default'"
+            @click="changeList('done')"
+          >已完成{{getupdatalength}}</a-button>
+        </a-button-group>
+        <a @click="clean">清除已完</a>
+      </div>
+    </a-list>
   </div>
 </template>
+<script>
+import { mapState, mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      //list: this.$store.state.list
+    };
+  },
+  computed: {
+    ...mapState(["inputValue", "valkey"]),
+    ...mapGetters(["getDonelength", "getupdatalength", "infoLits"])
+  },
+  created() {
+    this.$store.dispatch("getListA");
+  },
+  methods: {
+    changeValue(e) {
+      let val = e.target.value;
+      console.log(val);
+      this.$store.commit("changeValue", val);
+    },
+    addToList() {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning("请输入内容！");
+      }
+      this.$store.commit("addItemM");
+    },
+    // 根据 id 删除对应的数据
+    remove(id) {
+      console.log(id);
+
+      this.$store.commit("removeM", id);
+    },
+    // 监听e.target.checked
+    cbState(e, id) {
+      console.log(e.target.checked, id);
+      const params = {
+        id: id,
+        status: e.target.checked
+      };
+      this.$store.commit("Statechange", params);
+    },
+    // 清除已完成的数据
+    clean() {
+      this.$store.commit("cleanDoneM");
+    },
+    // 修改页面底部的active
+    changeList(key) {
+      console.log(key);
+
+      this.$store.commit("changeKey", key);
+    }
+  }
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 0;
+  margin-top: 50px;
+}
+#app .my_ipt {
+  width: 500px;
+  margin-left: 100px;
+}
+#app .dt_list {
+  width: 500px;
+  margin-top: 10px;
+  margin-left: 100px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,97 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
 Vue.use(Vuex)
 
+// import daa from ''
 export default new Vuex.Store({
   state: {
+    list: [], // 列表数据
+    inputValue: '',
+    nextId: 5,
+    valKey: 'all'
   },
+  getters: {
+    // 未完成
+    getDonelength(state) {
+      return state.list.filter(item => item.done === false).length;
+
+    },
+    // 已完成
+    getupdatalength(state) {
+      return state.list.filter(item => item.done === true).length;
+    },
+    // 
+    infoLits(state) {
+      if (state.valKey === 'all') {
+        return state.list
+      }
+      if (state.valKey === 'undone') {
+        return state.list.filter(item => !item.done)
+      }
+      if (state.valKey === 'done') {
+        return state.list.filter(item => item.done)
+      }
+      return state.list
+    }
+
+  }
+  ,
   mutations: {
+    getListM(state, res) {
+      state.list = res
+      console.log(state.list, 'lalla');
+    },
+    changeValue(state, val) {
+      state.inputValue = val
+    },
+    addItemM(state) {
+      const obj = {
+        id: state.nextId,
+        title: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    // 根据ID
+    removeM(state, id) {
+      const i = state.list.findIndex(item => item.id === id)
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+      //localStorage.setItem('data', JSON.stringify(state.list))
+    },
+    // 修改列表下的选中转态
+    Statechange(state, params) {
+      // 查找索引
+      const i = state.list.findIndex(item => item.id === params.id)
+      if (i !== -1) {
+        state.list[i].done = params.status
+      }
+    },
+    //// 清除已完成的数据
+    cleanDoneM(state) {
+      state.list = state.list.filter(item => item.done === false)
+    },
+    // 修改页面底部的active
+    changeKey(state, key) {
+      state.valKey = key
+
+    }
   },
   actions: {
+    getListA({ commit }, ) {
+      axios.get('http://localhost:3000/list').then((data) => {
+        let res = data.data
+        // localStorage.setItem('data', JSON.stringify(data.data))
+        console.log(res);
+        commit('getListM', res)
+      })
+    }
   },
   modules: {
   }
